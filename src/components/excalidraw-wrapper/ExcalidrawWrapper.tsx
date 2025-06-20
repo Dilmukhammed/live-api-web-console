e145b5067936513d51aae004734326e0a4a5e114
Dilmukhammed <152764509+Dilmukhammed@users.noreply.github.com>
2025-06-20 17:27:28 +0200

    Update ExcalidrawWrapper.tsx

--- src/components/excalidraw-wrapper/ExcalidrawWrapper.tsx
+++ src/components/excalidraw-wrapper/ExcalidrawWrapper.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Excalidraw } from '@excalidraw/excalidraw';
-import '@excalidraw/excalidraw/index.css';
 import './ExcalidrawWrapper.scss'; // To be created
 
 const ExcalidrawWrapper: React.FC = () => {
